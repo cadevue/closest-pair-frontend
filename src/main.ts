0b67f5b21fb5e52df7317b0a1e6c3074e92b10a9
Addin Munawwar <moonawar@protonmail.com>
2025-02-14 17:16:19 +0700

    feat: adjust sidebar style

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,25 @@ const MAX_NUM_OF_POINTS = 100000;
 const MIN_NUM_OF_POINTS = 1000;
 const INITIAL_NUM_OF_POINTS = 2000;
 
+const MIN_POS_BOUND = -100;
+const MAX_POS_BOUND = 100;
+
 let numOfPoints = INITIAL_NUM_OF_POINTS;
 let points : Array<Point> = new Array(MAX_NUM_OF_POINTS);
 
-function createScatterPlotRenderer() {
+const numOfPointsSlider = document.getElementById("numOfPoints") as HTMLInputElement;
+const numOfPointsValue = document.getElementById("numOfPointsValue") as HTMLSpanElement;
 
+numOfPointsSlider.min = MIN_NUM_OF_POINTS.toString();
+numOfPointsSlider.max = MAX_NUM_OF_POINTS.toString();
+numOfPointsSlider.value = INITIAL_NUM_OF_POINTS.toString();
+numOfPointsValue.innerHTML = numOfPointsSlider.value;
+
+numOfPointsSlider.oninput = function() {
+    numOfPoints = parseInt(numOfPointsSlider.value);
+    numOfPointsValue.innerHTML = numOfPoints.toString();
 }
+
+// function createScatterPlotRenderer(container : HTMLElement) {
+    
+// }
