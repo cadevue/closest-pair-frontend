96a58a8ddec8672f408949031242abc443273435
Addin Munawwar <moonawar@protonmail.com>
2025-02-14 16:23:36 +0700

    feat: base layout

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+type Point = { x: number, y: number, z: number }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const MAX_NUM_OF_POINTS = 100000;
+const MIN_NUM_OF_POINTS = 1000;
+const INITIAL_NUM_OF_POINTS = 2000;
+
+let numOfPoints = INITIAL_NUM_OF_POINTS;
+let points : Array<Point> = new Array(MAX_NUM_OF_POINTS);
